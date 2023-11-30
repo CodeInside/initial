bfec090d3957a70555c1e5898153e8ffba569f62
Alexey Zyablikov <35153440+a1xs@users.noreply.github.com>
2023-11-30 10:45:57 +0200

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -5,7 +5,7 @@ export ACCOUNT_ROOT='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAjAjYgaBI4qFgy1E8K0Fyl21
 #######
 
 export ACCOUNT_MAINTENANCE='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAq9T4yl4cUS6PWvKXkIafBQPmj6DmOPQTFEEHZUfuAYaCzTQq90caO3B4iBSQr0ps2aiE8KJ77h/6H/yb+7c1fCzpsOXBnTPWAXOLtAbZfrArgU1vM6IsbgQbNV7NJcaQr3waHze+BWDF5gZZH7E0OzKCw56hJigqLK2TWpAvZ4k4LYnFtuPDv1Uls+cc3q4baScapzjbYWU1+/jSXcDvehDutLHDVkuL6EA6KCPWN6T7qfG6YwL+jyh/FYacGP4Pe3pNSLRHnBlzz3PEduUXKHxHtYMJopXLpJIS5RLfaly2aP7aDtbvFyHtkpuaLJFdiwCxRwIOaNrh/FaBk+4a0wK2nEhy4egxd4Q5xMFWODoWx8UUU03NiCf2nxb1z2IRuMFQcx/mOTig4gUFLZXerCSuIntKD2JSUzMh4RiRt3OQKOAXX+9lZrCaWpjlkE0pOaQuv1IZF/OY/KX5NnhXSFd6KA/la5yNxEW7DjY3aM5eEHydoU4LicDDOlPC2YOUfoOPRmHevqrabH39gMadLaTaY/nZVooDmffirou+cg/sYsSlqR6v9iV/Bzp12zmK0M+A73U8vC9FtqtHmfxXu1V2YHFIKcvHBdi0rlBvi8kdLg/KPy2InUmLQdx9FHTcH5Yea1rfSzHohVlCcwJMMka0OqwSvKQyaPEVnb5Vhts= 4a:b1:be:df:1f:05:54:7e:be:47:9b:0f:05:a5:f6:c3 Maintenance account'
-export MAINTENANCE_HOME='/var/opt/maintenance'
+export MAINTENANCE_HOME='/home/maintenance'
 
 #################################################
 
